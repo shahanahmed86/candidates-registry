392bee90211428a78f0a4e7dfc54ee6a7de16dbe
Shahan <shahan.khaan@gmail.com>
2024-08-09 19:30:15 +0500

    refactor: moved db dependency from helper to database

--- database.py
+++ database.py
@@ -1,15 +1,21 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Annotated
+
+from fastapi import Depends
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from configs import configs
 
 
-def get_db():
-    url = f"mongodb://{configs.DB_USER}:{configs.DB_PASSWORD}@{configs.DB_HOST}:{configs.DB_PORT}/"
+async def get_db():
+    url = f"mongodb://{configs.DB_USER}:{configs.DB_PASSWORD}@{configs.DB_HOST}:{configs.DB_PORT}/{configs.DB_NAME}"
     mongodb_client = AsyncIOMotorClient(url)
     print("Database connected!")
     try:
-        db = mongodb_client.get_database(configs.DB_NAME)
+        db = mongodb_client.get_database()
         yield db
     finally:
         print("Database connection closed!")
         mongodb_client.close()
+
+
+db_dependency = Annotated[AsyncIOMotorDatabase, Depends(get_db)]
--- helper.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from motor.motor_asyncio import AsyncIOMotorDatabase
-
-from database import get_db
-
-db_dependency = Annotated[AsyncIOMotorDatabase, Depends(get_db)]
