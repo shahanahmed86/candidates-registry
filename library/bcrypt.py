3089fbac8e104ef6d03bdcb89dea6efadba2859c
Shahan <shahan.khaan@gmail.com>
2024-08-09 19:32:28 +0500

    fix: removed salt from bcrypt settings

--- library/bcrypt.py
+++ library/bcrypt.py
@@ -1,13 +1,11 @@
 from passlib.context import CryptContext
 
-from configs import configs
-
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash(value: str) -> str:
-    return bcrypt_context.hash(configs.BCRYPT_SALT, value)
+    return bcrypt_context.hash(value)
 
 
 def verify(hashed_value: str, value: str) -> bool:
-    return bcrypt_context.verify(configs.BCRYPT_SALT, hashed_value, value)
+    return bcrypt_context.verify(hashed_value, value)
