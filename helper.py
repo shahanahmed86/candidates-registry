4abadf973f9df0037236323f6d2a5ffc13afa49b
Shahan <shahan.khaan@gmail.com>
2024-08-11 19:07:09 +0500

    feat: created utils funcs

---
+++ helper.py
@@ -0,0 +1,21 @@
+from bson import ObjectId
+from fastapi import HTTPException, status
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+
+def format_response(message: str) -> str:
+    return {
+        "description": message,
+        "content": {"application/json": {"example": {"detail": message}}},
+    }
+
+
+async def check_validaty_and_existance(coll: AsyncIOMotorCollection, _id: str) -> dict:
+    if not ObjectId.is_valid(_id):
+        raise HTTPException(status.HTTP_406_NOT_ACCEPTABLE, "The ID was invalid!")
+
+    record = await coll.find_one({"_id": ObjectId(_id)})
+    if not record:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Candidate not found!")
+
+    return record
