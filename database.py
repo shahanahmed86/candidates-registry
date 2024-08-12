6d6b3f799037d3750c92819a3f639403b0bb87d5
Shahan <shahan.khaan@gmail.com>
2024-08-12 21:33:04 +0500

    fix: make get db function as a non-coroutine

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from configs import configs
 
 
-async def get_db():
+def get_db():
     url = f"mongodb://{configs.DB_USER}:{configs.DB_PASSWORD}@{configs.DB_HOST}:{configs.DB_PORT}/{configs.DB_NAME}"
     mongodb_client = AsyncIOMotorClient(url)
     print("Database connected!")
